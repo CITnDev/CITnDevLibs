5706c8a431140c0bf8d80bf1560d5be9223ee870
CitnDev <citndev@gmail.com>
2012-05-15 20:10:08 +0200

    Add CitnDev.System.Extensions.ObjectExtensions to convert an object to another

---
+++ CitnDev.System_4.0/Extensions/ObjectExtensions.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace CitnDev.System.Extensions
+{
+    public static class ObjectExtensions
+    {
+        #region Map properties
+
+        //
+        // Source Type, Destination Type, Map methods
+        //
+        private static readonly Dictionary<Type, Dictionary<Type, List<Action<object, object>>>> Mappings = new Dictionary<Type, Dictionary<Type, List<Action<object, object>>>>();
+
+        public static TU To<TU>(this object sourceInstance)
+        {
+            var sourceType = sourceInstance.GetType();
+            var returnInstance = Activator.CreateInstance<TU>();
+            var generateMapping = false;
+
+            lock (Mappings)
+            {
+
+                if (!Mappings.ContainsKey(sourceType))
+                {
+                    generateMapping = true;
+                    Mappings[sourceType] = new Dictionary<Type, List<Action<object, object>>>();
+                }
+
+                if (!Mappings[sourceType].ContainsKey(typeof(TU)))
+                {
+                    generateMapping = true;
+                    Mappings[sourceType][typeof(TU)] = new List<Action<object, object>>();
+                }
+            }
+
+            #region Generation getter->setter
+
+            if (generateMapping)
+            {
+                var sourceProperties = sourceType.GetProperties(BindingFlags.Instance | BindingFlags.FlattenHierarchy | BindingFlags.GetProperty | BindingFlags.Public);
+                var destinationProperties = typeof(TU).GetProperties(BindingFlags.Instance | BindingFlags.FlattenHierarchy | BindingFlags.SetProperty | BindingFlags.Public);
+                var mapping = new Dictionary<PropertyInfo, PropertyInfo>();
+
+                foreach (var sourceProperty in sourceProperties)
+                {
+                    var destinationProperty = destinationProperties.FirstOrDefault(p =>
+                            (p.PropertyType.IsEnum && sourceProperty.PropertyType.IsEnum || p.PropertyType == sourceProperty.PropertyType)
+                            && p.Name == sourceProperty.Name && p.CanWrite
+                        );
+                    if (destinationProperty != null)
+                    {
+                        mapping.Add(sourceProperty, destinationProperty);
+                    }
+                }
+
+
+                foreach (var propertyInfo in mapping.Keys)
+                {
+                    lock (Mappings)
+                    {
+                        Mappings[sourceType][typeof(TU)].Add(GenerateCastProperty(sourceType, typeof(TU), propertyInfo, mapping[propertyInfo]));
+                    }
+                }
+            }
+
+            #endregion
+
+            foreach (var action in Mappings[sourceType][typeof(TU)])
+            {
+                action(returnInstance, sourceInstance);
+            }
+
+            return returnInstance;
+        }
+
+        public static Action<object, object> GenerateCastProperty(Type sourceType, Type destinationType, PropertyInfo propSource, PropertyInfo propDest)
+        {
+            var destParameter = Expression.Parameter(typeof(object), "instance");
+            var sourceParameter = Expression.Parameter(typeof(object), "param");
+
+            var propertyGetExpression = Expression.Property(Expression.Convert(sourceParameter, sourceType), propSource);
+            var propertySetExpression = Expression.Property(Expression.Convert(destParameter, destinationType), propDest);
+
+            Expression assignExpression;
+            if (propSource.PropertyType.IsEnum)
+                assignExpression = Expression.Assign(propertySetExpression, Expression.Convert(propertyGetExpression, propDest.PropertyType));
+            else
+                assignExpression = Expression.Assign(propertySetExpression, propertyGetExpression);
+
+            var lambda = Expression.Lambda<Action<object, object>>(assignExpression, destParameter, sourceParameter);
+
+            return lambda.Compile();
+        }
+
+        #endregion
+    }
+}
---
+++ CitnDev.System_Test/Extensions/ObjectExtensionsTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CitnDev.System.Extensions;
+using NUnit.Framework;
+
+namespace CitnDev.System_Test.Extensions
+{
+    [TestFixture]
+    public class ObjectExtensionsTests
+    {
+        public enum Enum1 { Ok = 0, Ko = 1 }
+        public enum Enum2 { Ok = 0, Ko = 1 }
+
+        public class BaseClass1
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public Enum1 Status { get; set; }
+        }
+
+        public class Class1 : BaseClass1
+        {
+            public List<string> Data { get; set; }
+            public Dictionary<long, string> Dict { get; set; }
+        }
+
+        public class Class2
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public Enum1 Status { get; set; }
+            public Dictionary<long, string> Dict { get; set; }            
+        }
+
+        [Test]
+        public void ObjetctExtensionToTest()
+        {
+            var class2 = new Class2 { Id = 1234, Name = "Class2", Dict = new Dictionary<long, string>(), Status = Enum1.Ko };
+            var class1 = class2.To<Class1>();
+
+            Assert.True(class1.Dict == class2.Dict);
+            Assert.True(class1.Id == class2.Id);
+            Assert.True(class1.Name == class2.Name);
+            Assert.True((int)class1.Status == (int)class2.Status);
+        }
+
+    }
+}
